bc735da75382e7d4a3e99e2c575b83f532370548
sanatan-dive <sanatansharma352@gmail.com>
2024-11-07 22:37:04 +0530

    prisma worked

---
+++ lib/prisma.ts
@@ -0,0 +1,18 @@
+import {PrismaClient} from '@prisma/client'
+import exp from 'constants'
+
+const prismaClientSiglton = ()=>{
+    return new PrismaClient()
+}
+
+const globalForPrisma = globalThis as unknown as {prisma: PrismaClient|undefined}
+
+ const prisma = globalForPrisma.prisma ?? prismaClientSiglton()
+
+
+
+ if(process.env.NODE_ENV !== 'production'){
+    globalForPrisma.prisma = prisma
+ }
+
+ export default prisma
